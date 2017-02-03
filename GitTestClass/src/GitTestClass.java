95cfb44f09c956ad3bab19fca359ee18c31127c8
bojark <kosstin@gmail.com>
2017-02-03 12:55:49 +0300

    Публикация тестового класса на java для Гита

---
+++ GitTestClass/src/GitTestClass.java
@@ -0,0 +1,8 @@
+/**
+ * Класс для экспериментов с гитом.
+ */
+public class GitTestClass {
+    public static void main(String[] args) {
+        System.out.println("Это тестовая прога для взаимодействия с Гитом!");
+    }
+}
