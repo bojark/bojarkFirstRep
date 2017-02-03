1121cff1b3e28c3e83eb633fb2e1bd09d1bc4d59
bojark <kosstin@gmail.com>
2017-02-03 13:19:47 +0300

    перенёс тестовый код в отдельный метод

--- GitTestClass/src/GitTestClass.java
+++ GitTestClass/src/GitTestClass.java
@@ -3,7 +3,11 @@
  */
 public class GitTestClass {
     public static void main(String[] args) {
+
+    }
+
+    public static void metod(){
         System.out.println("Это тестовая прога для взаимодействия с Гитом!");
-		System.out.println("Это уже прямо-таки вторая версия, что ты!");
+        System.out.println("Это уже прямо-таки вторая версия, что ты!");
     }
 }
